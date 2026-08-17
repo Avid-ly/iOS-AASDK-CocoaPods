304589d0dde9601911131d40f9f61a675bdf3388
steve <909476821@qq.com>
2026-08-17 14:32:12 +0800

    添加 SPM 支持

---
+++ Framework/AASDK/AASAccount.xcframework/ios-arm64/AASAccount.framework/Headers/AASAccount.h
@@ -0,0 +1,15 @@
+//
+//  AASAccount.h
+//  AASAccount
+//
+//  Created by steve on 2026/8/17.
+//  Copyright © 2026 何健. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#import "AASDK.h"
+#import "AASDKAppDelegate.h"
+#import "AALoginModel.h"
+#import "AASDKLoginView.h"
+#import "AASDKLoginAPI.h"
